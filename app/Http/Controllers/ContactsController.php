f0989854613be755215d9e0f0bd0e0e983ec6a58
= <=>
2019-10-31 19:37:13 +0500

    Messenger Chat app styling

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Message;
+
+class ContactsController extends Controller
+{
+    public function get(){
+
+        $contacts = User::all();
+
+        return response()->json($contacts);
+    }
+
+    public function getMessagesFor($id){
+
+        $messages = Message::where('from', $id)->orWhere('to', $id)->get();
+
+        return response()->json($messages);
+    }
+}
---
+++ resources/assets/js/components/ChatApp.vue
@@ -0,0 +1,56 @@
+<template>
+    
+    <div class="chat-app">
+        <Conversation :contact="selectedContact" :messages="messages"/>
+        <ContactsList :contacts="contacts" @selected="startConversationWith"/>
+    </div>
+</template>
+
+<script>
+
+    import Conversation from './Conversation';
+    import ContactsList from './ContactsList';
+
+    export default {
+
+        props: {
+            user: {
+                type: Object,
+                required: true
+            }
+        },
+        data(){
+            return {
+                selectedContact: null,
+                messages: [],
+                contacts: []
+            }
+        },
+        mounted() {
+            console.log(this.user);
+            
+            axios.get('/contacts')
+                .then((response) => {
+                    console.log(response.data);
+                    
+                    this.contacts = response.data;
+                });
+        },
+        methods:{
+            startConversationWith(contact){
+                axios.get(`/conversation/${contact.id}`)
+                    .then((response) =>{
+                        this.messages = response.data
+                        this.selectedContact = contact
+                    });
+            }
+        },
+        components: {Conversation, ContactsList}
+    }
+</script>
+
+<style lang="scss" scoped>
+    .chat-app{
+        display: flex;
+    }
+</style>
